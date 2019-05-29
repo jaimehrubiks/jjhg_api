7c4f697323a4755595782fd384a9a8f7226aacbe
jaimehrubiks <jaimehidalgo2@hotmail.com>
2019-05-29 13:13:18 -0500

    a

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000
 const version = require('../package.json').version
 
-app.get('/', (req, res) => res.send('jjhg API running'));
+app.get('/', (req, res) => res.send('jjhg API running.'));
 app.get('/version', (req, res) => res.send(version));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
